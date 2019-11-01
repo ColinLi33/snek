b574aa754eb3c463dfa682f3941f8833646dd571
Colin Li <213003@NA200-A001465.local>
2019-11-01 14:55:56 -0500

    dsjgk

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index.html'))
 
-app.listen(process.env.PORT || 3333, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 3333, () => console.log('Example app listening'))
