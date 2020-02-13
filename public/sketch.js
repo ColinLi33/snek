1514ee9866e31fa5f6b88b667f2025f90c6f8cae
ColinLi33 <Colin8tor1206@gmail.com>
2020-02-13 15:45:29 -0600

    2 player

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,11 @@
 canvasWidth = 460;
-canvasHeight = 460;
+canvasHeight = 460; //460
 let snake;
 let snake2;
 let apple;
+let winString;
+let currCanv = 1;
+let winText;
 /*var socket = io.connect("localhost:3333");
 
 socket.on('player', function(data){
@@ -11,20 +14,21 @@ socket.on('player', function(data){
 });*/
 //You got this!
 function setup() {
-  frameRate(20);
-  createCanvas(canvasWidth * 2, canvasHeight * 2);
-  snake = new Snake(randomizer(canvasWidth), randomizer(canvasHeight), 20);
+  frameRate(13);
+  createCanvas(canvasWidth + 50, canvasHeight + 50);
+  snake = new Snake(randomizer(canvasWidth), randomizer(canvasHeight), 20, 1);
+  snake2 = new Snake(randomizer(canvasWidth), randomizer(canvasHeight), 20, 2);
   apple = new Apple(randomizer(canvasWidth), randomizer(canvasHeight), 20);
 }
-
+/*
 function displayScore() {
   let s = "Score: " + snake.getScore();
   fill(50);
   textSize(20);
   noStroke();
   text(s, canvasWidth - 260, canvasHeight + 20);
-  clear
-}
+
+}*/
 
 function randomizer(thing) {
     let randomNumber = Math.random() * thing;
@@ -32,20 +36,45 @@ function randomizer(thing) {
 }
 
 function draw(){
-  background(255);
+if(currCanv == 1){
+//  background(255);
+  fill(75);
   rect(0, 0, canvasWidth, canvasHeight);
-
   apple.show();
   snake.move();
+  snake2.move();
   snake.wall();
-  snake.collision();
+  snake2.wall();
+  snake.collision(snake2);
+  snake2.collision(snake);
+  if(snake.win == 1){
+    setup();
+    winString = "Player 1 Wins!\nPress space to play again."
+    currCanv = 2;
+  } else if(snake.win == 2){
+    setup();
+    winString = "Player 2 Wins!\nPress space to play again."
+    currCanv = 2;
+  } else if(snake.win == 3){
+    setup();
+    winString = "Tie Game!\nPress space to play again."
+    currCanv = 2;
+  }
   snake.trail();
+  snake2.trail();
   snake.eat(apple);
+  snake2.eat(apple);
   snake.show();
+  snake2.show();
+//  background(255);
+
+} else if(currCanv == 2){
   textAlign(CENTER, CENTER);
   fill(75);
+  rect(0, 0, canvasWidth, canvasHeight);
   textSize(20);
-  text("Score: " + snake.score, canvasWidth - 260, canvasHeight + 20);
+  text(winString, canvasWidth - 260, canvasHeight + 24)
+}
 
 /*
   socket.emit('snake1', snake);
@@ -54,14 +83,30 @@ function draw(){
 }
 
 function keyPressed() {
-  if (keyCode === UP_ARROW && snake.dir != 'DOWN') {
+  if (keyCode === UP_ARROW && snake2.dir != 'DOWN') {
+    snake2.dir = 'UP'
+  } else if (keyCode === DOWN_ARROW && snake2.dir != 'UP') {
+    snake2.dir = 'DOWN';
+  }
+  if (keyCode === LEFT_ARROW && snake2.dir != 'RIGHT') {
+    snake2.dir = 'LEFT';
+  } else if (keyCode === RIGHT_ARROW && snake2.dir != 'LEFT') {
+    snake2.dir = 'RIGHT';
+  }
+
+  if (keyCode === 87 && snake.dir != 'DOWN') {
     snake.dir = 'UP'
-  } else if (keyCode === DOWN_ARROW && snake.dir != 'UP') {
+  } else if (keyCode === 83 && snake.dir != 'UP') {
     snake.dir = 'DOWN';
   }
-  if (keyCode === LEFT_ARROW && snake.dir != 'RIGHT') {
+  if (keyCode === 65 && snake.dir != 'RIGHT') {
     snake.dir = 'LEFT';
-  } else if (keyCode === RIGHT_ARROW && snake.dir != 'LEFT') {
+  } else if (keyCode === 68 && snake.dir != 'LEFT') {
     snake.dir = 'RIGHT';
+  }
+
+  if(keyCode === 32 && currCanv == 2){
+    currCanv = 1;
+    setup();
   }
 }
--- public/snake.js
+++ public/snake.js
@@ -5,15 +5,23 @@ class Snake {
 	tail = [];
 	xVel = 0;
 	yVel = 0;
+	player = 0;
+	win = 0; //1 = p1 wins, 2 = p2 wins, 3 = tie
 
-	constructor(x, y, size) {
+	constructor(x, y, size, player) {
 		this.x = x;
 		this.y = y;
 		this.size = size;
+		this.player = player;
+		this.win = null;
+		this.tail = [];
 	}
 
 	show() {
-		fill('#00ff00');
+		if(this.player == 1)
+			fill('#00ff00');
+		if(this.player == 2)
+			fill('#FFA500');
 		rect(this.x, this.y,this.size, this.size);
 	//console.log(this.tail[0]);
 		for(let i = 0; i < this.score; i++){
@@ -21,11 +29,6 @@ class Snake {
 			//console.log(this.tail[i].y);
 			rect(this.tail[i].x, this.tail[i].y, this.size, this.size);
 		}
-	}
-
-	reset(){
-		this.tail = [];
-		this.score = 1;
 	}
 
 	trail() {
@@ -43,13 +46,41 @@ class Snake {
 		}
 	}
 
-	collision(){
-		for(let i = 1; i < this.score; i++){
-			if(this.x + this.size > this.tail[i].x && this.x < this.tail[i].x + this.size && this.y < this.tail[i].y + this.size && this.y + this.size > this.tail[i].y){
-				this.reset();
+	randomizer(thing) {
+	    let randomNumber = Math.random() * thing;
+	    return randomNumber - (randomNumber % 20);
+	}
+
+	collision(snake){
+		/*
+		for(let i = 1; i < snake.score; i++){
+			if(this.x + this.size > snake.tail[i].x && this.x < snake.tail[i].x + this.size && this.y < snake.tail[i].y + snake.size && this.y + this.size > snake.tail[i].y){
+				this.lose();
+			}
+		}*/
+		var d = dist(this.x, this.y, snake.x, snake.y);
+		if(d < 10){
+			console.log("tie game");
+			this.win = 3;
+	//		lose();
+		}
+		for (var i = 1; i < this.score; i++) {
+			d = dist(this.x, this.y, this.tail[i].x, this.tail[i].y);
+			if(d < 10){
+				console.log("PLAYER 1 WINS");
+				this.win = 1;
+			//	lose();
+			}
+		}
+		for (var i = 1; i < snake.score; i++) {
+			d = dist(this.x, this.y, snake.tail[i].x, snake.tail[i].y);
+			if(d < 10){
+				console.log("PLAYER 2 WINS");
+				this.win = 2;
+			//	lose();
+			}
 		}
 	}
-}
 
 	move() {
 		if(this.dir == 'RIGHT')
@@ -61,18 +92,19 @@ class Snake {
 		 if(this.dir == 'DOWN')
 			this.y += this.size;
 	}
-
   wall(){
     if(this.x <= 0 - this.size)
       this.x = 460 - this.size;
+	//		this.reset();
 
     if(this.y <= 0 - this.size)
       this.y = 460 - this.size;
-
+	//		this.reset();
     if(this.x >= 460)
       this.x = 0;
-
+	//		this.reset();
     if(this.y >= 460)
       this.y = 0;
+	//		this.reset();
     }
 }
